6b7e7672748fea43a253b9b2f50149ac0f8e9ae7
SteveChiru <cqr151623@163.com>
2019-01-06 20:57:32 +0800

    learn two methods of search document tree: find_all() and find()

--- webcrawler/reference_database/how_to_use_bs.py
+++ webcrawler/reference_database/how_to_use_bs.py
@@ -4,7 +4,32 @@
 
 from bs4 import BeautifulSoup
 from lxml import html
+import re
 
 soup = BeautifulSoup(open("testbs.html"),"lxml")
-print(soup) #输出html文件全部内容
+#print(soup) #输出html文件全部内容
 
+"""
+搜索文档树
+1.find_all(name,attrs,recursive,string,**kwargs)
+2.find(name,attrs,recursive,string,**kwargs)
+"""
+
+###使用find_all()方法###
+#name参数
+#print(soup.find_all('title'))   #获取所有的a标签
+
+#keyword参数
+#print(soup.find_all(id='link2'))    #获取id是link2的标签
+#print(soup.find_all(href=re.compile("elsie")))  #使用正则
+#print(soup.find_all(href=re.compile("elsie"),id="link1"))   #使用多个指定名字的参数
+
+#按CSS搜索
+print(soup.find_all("a",class_="sister"))   #class 在Python中是保留字,使用 class 做参数会导致语法错误
+
+###使用find()方法###
+"""
+find_all()方法和find()方法的区别：
+1.find_all() 方法的返回结果是值包含一个元素的列表,而 find() 方法直接返回结果.
+2.find_all() 方法没有找到目标是返回空列表, find() 方法找不到目标时,返回 None .
+"""
