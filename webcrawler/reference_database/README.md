b08a926c654a9680b120194b604854edd0c67c1a
SteveChiru <cqr151623@163.com>
2019-01-06 20:09:51 +0800

    add readme.md;add testbs.html;modity the environment of learn bs

---
+++ webcrawler/reference_database/README.md
@@ -0,0 +1,2 @@
+This directory created to learn and test some modules about Python.
+1.how_to_use_bs.py : learn how to use beautiful_soup
--- webcrawler/reference_database/how_to_use_bs.py
+++ webcrawler/reference_database/how_to_use_bs.py
@@ -3,28 +3,8 @@
 """
 
 from bs4 import BeautifulSoup
+from lxml import html
 
-html_doc = """
-<html>
-<head>
-    <title>The Dormouse's story</title>
-</head>
-
-<body>
-    <p class="title"><b>The Dormouse's story</b></p>
-
-    <p class="story">Once upon a time there were three little sisters; and their names were
-    <a href="http://example.com/elsie" class="sister" id="link1">Elsie</a>,
-    <a href="http://example.com/lacie" class="sister" id="link2">Lacie</a> and
-    <a href="http://example.com/tillie" class="sister" id="link3">Tillie</a>;
-    and they lived at the bottom of a well.</p>
-
-    <p class="story">...</p>
-
-</body>
-
-</html>    
-"""
-
-soup = BeautifulSoup(html_doc, 'html.parser')   #获取到html格式的文件
+soup = BeautifulSoup(open("testbs.html"),"lxml")
 print(soup) #输出html文件全部内容
+
