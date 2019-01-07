da8f80113f7f18dd37c856ce4dcc1206737fd108
SteveChiru <cqr151623@163.com>
2019-01-07 17:56:53 +0800

    almost perfect except the result excel file has empty rows(废æ(废æ(withdrown standards turn to be)

--- webcrawler/GuoBiaoShuYu.py
+++ webcrawler/GuoBiaoShuYu.py
@@ -1,6 +1,6 @@
 """
 需求：
-    1.爬取国家标准全文公开系统上公开的标准，转换成excel数据
+    1.爬取国家标准全文公开系统上公开的现行和即将实施的标准，转换成excel数据
     2.excel文件格式 标准号-中文标准名称-英文标准名称-标准状态-在线预览链接-下载链接
 细节设计：
     1.页面中如果没有在线预览按钮，说明无法在线预览，excel表中填入“无法在线预览”
@@ -13,12 +13,13 @@ import xml
 import requests
 import re
 import xlwt
+import urllib.parse
 
 """
 使用xlwt模块初始化一个workbook
 返回值：初始化后的workbook
 """
-def initWorkBook():
+def initWorkBook(category):
     workbook = xlwt.Workbook(encoding='utf-8')
     return workbook
 
@@ -31,7 +32,6 @@ def createWorkSheet(sheetname, workbook = xlwt.Workbook):
     worksheet.write(0,0,'标准号'); worksheet.write(0,1,'中文标准名称')
     worksheet.write(0,2,'英文标准名称');worksheet.write(0,3,'标准状态')
     worksheet.write(0,4,'在线预览url');worksheet.write(0,5,'下载url')
-
     return worksheet
 
 """
@@ -71,13 +71,35 @@ def getDownloadUrl(filenumber,soup):
 """
 获取一个页面上的数据
 """
-def getDataOfOnePage(pagenumber,datas):
-    index = 0
+
+
+def getStandardStatus(beautifulsoup):
+    standard_status_class = beautifulsoup.find('table', class_='tdlist').find_all('tr')[2].find('span').get('class')[0]
+    standard_status = '现行'
+    if standard_status_class == 'text-warning':
+        standard_status = '即将实施'
+    if standard_status_class == 'text-danger':
+        standard_status = '废止'
+    return standard_status
+
+def getDataOfOnePage(pagenumber,datas,worksheet,category):
+    datatotal = getDataTotal(category)
+    pagetotal = getPageTotal(datatotal)
+    data_number_per_page = 10
     row = 1 + (pagenumber-1)*10
-    while index < 20:
+    if pagenumber is pagetotal:
+        data_number_per_page = int(datatotal)%10
+
+    index = 0
+    index_total = 2 * data_number_per_page
+    while index < index_total:
         filenumber = datas[index].get('onclick')[10:-3]
         new_gb_info_url = 'http://www.gb688.cn/bzgk/gb/newGbInfo?hcno=' + filenumber   #标准信息页面
         soup = creatSoup(new_gb_info_url)
+        standard_status = getStandardStatus(soup)
+        if standard_status == '废止': #当标准状态为废止时，就不用保存数据到excel中
+            index = index + 2
+            continue
         #标准号-中文标准名称-英文标准名称-标准状态-在线预览链接-下载链接
         standard_text = soup.find('h1').get_text()
         standard_number = standard_text[4:]
@@ -85,7 +107,6 @@ def getDataOfOnePage(pagenumber,datas):
             standard_number = standard_text[4:-2]
         standard_chi_name = soup.find('table',class_='tdlist').find('b').string
         standard_eng_name = soup.find('table',class_='tdlist').find_all('td')[2].string[7:]
-        standard_status = soup.find('span',class_='text-success').string
         urlonlinereadurl = getOnlineReadUrl(filenumber,soup)    #处理在线阅读链接
         downloadurl = getDownloadUrl(filenumber,soup)    #处理下载标准链接
 
@@ -102,30 +123,65 @@ def getDataOfOnePage(pagenumber,datas):
 """
 获取所需要的所有数据
 """
-def getAllData(pagetotal):
+
+"""
+获取给定类别的数据总共有多少页
+"""
+def getDataTotal(category):
+    category_to_url = urllib.parse.quote(category)
+    category_url = 'http://www.gb688.cn/bzgk/gb/std_list?r=0.5129843308028312&page=1&pageSize=10&p.p1=0&p.p2='+category_to_url+'&p.p90=circulation_date&p.p91=desc'
+    soup = creatSoup(category_url)
+    standard_text = soup.find_all('td',class_='hidden-sm hidden-xs')[0].find('span').get_text()
+    datatotal = re.findall(r"[共]\xa0(.+?)\xa0[条]",standard_text)[0]
+    return datatotal
+
+"""
+获取总数据的总页数
+"""
+def getPageTotal(datatotal):
+    pagetotal = int(datatotal)//10 + 1
+    return pagetotal
+
+def getAllData(workbook,category):
+    pagetotal = getPageTotal(getDataTotal(category))  #获取指定类别的数据的总页数
+    savepath = setSavePath(category)    #设置要文件存储路径
+    worksheet = createWorkSheet(category,workbook)
+    category_url = urllib.parse.quote(category) #类别从汉字转为url编码
     pageindex = 1
     while pageindex <= pagetotal:
-        url = "http://www.gb688.cn/bzgk/gb/std_list?page="+str(pageindex)+"&pageSize=10&p.p1=0&p.p2=%E8%88%B9%E8%88%B6&p.p5=PUBLISHED&p.p90=circulation_date&p.p91=desc"
-        f = requests.get(url)                 #Get该网页从而获取该html内容
-        soup = BeautifulSoup(f.content, "lxml")  #用lxml解析器解析该网页的内容, 好像f.text也是返回的html
+        url = 'http://www.gb688.cn/bzgk/gb/std_list?page='+str(pageindex)+'&pageSize=10&p.p1=0&p.p2=' +category_url+ '&p.p90=circulation_date&p.p91=desc'
+        soup = creatSoup(url)
         table = soup.find('table',class_='table result_list table-striped table-hover')
         datas = table.find_all('a')
 
         #   把一个页面上的信息存入excel文件内
-        getDataOfOnePage(pageindex,datas)
+
+        getDataOfOnePage(pageindex,datas,worksheet,category)
         pageindex = pageindex + 1
+
+    saveToExcel(savepath,workbook)  #把数据保存到excel文件内
+    print("共"+str(pagetotal)+"页数据爬取完毕") #信息提示
+
+"""
+设置要爬取的数据的类别
+"""
+def setCategory(category):
+    return category
+
+"""
+设置数据要存储的路径
+"""
+def setSavePath(category):
+    savepath = 'D:\databank\爬虫\国标术语\国标术语-'+category+'.xls'  #设置excel文件存储路径
+    return savepath
 
 #函数入口
 if __name__ == "__main__":
 
     #   设置要爬取的数据的参数
-    category = '船舶'     #设置要爬取的术语的类别
-    pagetotal = 2          #设置要爬取的数据总共有多少页
-    path = 'D:\databank\爬虫\国标术语\国标术语-'+category+'.xls'  #设置excel文件存储路径
+    category = setCategory('船舶')
+    workbook = initWorkBook(category)       # 创建一个workbook 设置编码
 
-    workbook = initWorkBook()       # 创建一个workbook 设置编码
-    worksheet = createWorkSheet(category, workbook) # 创建一个worksheet
-    getAllData(pagetotal)   #爬取所有数据
-    saveToExcel(path,workbook)  #把数据保存到excel文件内
+    getAllData(workbook,category)   #爬取所有数据
 
-    print("共"+str(pagetotal)+"页数据爬取完毕") #信息提示
+
